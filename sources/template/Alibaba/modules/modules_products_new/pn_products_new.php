9e3866e3397ed10d40d6e93affbda75d98d68624
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-07-20 10:17:41 -0400

    oups !

--- sources/template/Alibaba/modules/modules_products_new/pn_products_new.php
+++ sources/template/Alibaba/modules/modules_products_new/pn_products_new.php
@@ -304,7 +304,6 @@
               $new_prods_content .= '<div class="col-md-6 pagenumber hidden-xs">';
               $new_prods_content .=  $Qlisting->getPageSetLabel(CLICSHOPPING::getDef('text_display_number_of_items'));
               $new_prods_content .= '</div>';
-              $new_prods_content .= 'tttt';
               $new_prods_content .= '<div class="col-md-6 float-md-right">';
               $new_prods_content .= '<span class="float-md-right pagenav">'.  $Qlisting->getPageSetLinks(CLICSHOPPING::getAllGET(array('page', 'info', 'x', 'y')), 'Shop') . '</span>';
               $new_prods_content .= '<span class="text-md-right">' . CLICSHOPPING::getDef('text_result_page') . '</span>';
